f1061bc63de2b029c14c26701f27465a0bcfb56b
Ahmad Hurayarah <hurayarah@gmail.com>
2024-08-05 11:36:43 +0500

    seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,20 @@ import { ThemeProvider } from "@/components/providers/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Edurup",
-  description: "",
+  title: "Edurup - Empowering Your Future",
+  description:
+    "Edurup offers top-notch courses and training programs to help you achieve your career goals. Join us to learn from industry experts and get job-ready skills in the most sought-after fields.",
+  keywords: [
+    "Edurup",
+    "Online Courses",
+    "Career Training",
+    "MERN Stack",
+    "Digital Marketing",
+    "Job Placement",
+    "Industry Experts",
+  ],
+  authors: [{ name: "Edurup", url: "https://www.edurup.com" }],
+  viewport: "width=device-width, initial-scale=1.0",
   icons: {
     icon: [
       {
@@ -22,16 +34,57 @@ export const metadata: Metadata = {
       },
     ],
   },
+  openGraph: {
+    title: "Edurup - Empowering Your Future",
+    description:
+      "Join Edurup to learn from industry experts and gain the skills needed to excel in your career.",
+    url: "https://www.edurup.com",
+    siteName: "Edurup",
+    images: [
+      {
+        url: "/og_image.png",
+        width: 1200,
+        height: 630,
+        alt: "Edurup",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Edurup - Empowering Your Future",
+    description:
+      "Join Edurup to learn from industry experts and gain the skills needed to excel in your career.",
+    images: "/twitter_image.png",
+    site: "@EdurupSchool",
+  },
+  robots: "index, follow",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body>
+      <head>
+        <meta name="theme-color" content="#000000" />
+        <meta name="msapplication-navbutton-color" content="#000000" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+        <meta name="application-name" content="Edurup" />
+        <meta name="apple-mobile-web-app-title" content="Edurup" />
+        <meta
+          name="msapplication-tooltip"
+          content="Edurup - Empowering Your Future"
+        />
+        <meta name="format-detection" content="telephone=no" />
+      </head>
+      <body className={inter.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
