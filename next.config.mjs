cfd771513d128e0b1c5478c61fb3545e81cb6f56
Ahmad Hurayarah <hurayarah@gmail.com>
2024-08-06 03:29:52 +0500

    job roles

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true, // Disable the default Image Optimization
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
