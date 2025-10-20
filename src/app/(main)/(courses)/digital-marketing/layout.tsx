01b538a42c28e48715e96d62ff605a7f007ed1f4
therupeshneelam-edu <therupeshneelam@gmail.com>
2025-10-20 23:10:55 +0530

    Update layout.tsx

--- src/app/(main)/(courses)/digital-marketing/layout.tsx
+++ src/app/(main)/(courses)/digital-marketing/layout.tsx
@@ -2,8 +2,8 @@ import { Navbar } from "@/components/Navbar";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Digital Marketing",
-  description: "",
+  title: "PGP in Digital Marketing in Bangalore | Edurup Learning - SEO, Ads & Analytics Training",
+  description: "Join Edurup Learning’s Post Graduate Program in Digital Marketing in Bangalore. Get hands-on training in SEO, Google Ads, Social Media, and Analytics from real-time MNC professionals with 100% placement support.",
 };
 
 const CoursesLayout = ({ children }: { children: React.ReactNode }) => {
