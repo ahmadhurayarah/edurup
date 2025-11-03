b4703e111aed8e3123ab2a789f7ddcbacfed1579
Malkan <malkanshaheen45@gmail.com>
2025-11-03 15:28:26 +0500

    header error resolved

--- src/app/(main)/components/data-analytics/meta.js
+++
@@ -1,5 +0,0 @@
-export const meta = {
-  title: "Data Analytics Course",
-  description:
-    "Join the best data analytics course in your city with Edurup’s expert-led sessions, projects, and placement support.",
-};
